216faa30defece4a73195e5bbce52b5014013531
Jurgen desmidt <jurgen.desmidt@gmail.com>
2023-04-25 16:19:57 +0200

    correction on classpath in sh file

--- documenttoolkit.sh
+++ documenttoolkit.sh
@@ -1,2 +1,2 @@
 cd /java/documenttoolkit
-java -cp "documenttoolkit-1.0.0.jar:*"  eu.cdinvest.documenttoolkit.DocumentToolkit
+java -cp "documenttoolkit-1.0.0.jar:/lib/*:*"  eu.cdinvest.documenttoolkit.DocumentToolkit
